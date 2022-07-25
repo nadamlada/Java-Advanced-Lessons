e83985a4e0569d0d5198e87e8479f5911cc4e307
Nadezhda <nan.dudova@gmail.com>
2022-07-25 16:10:50 +0300

    commits

--- src/JA02StacksAndQueuesExercises/X05BalancedParentheses.java
+++ src/JA02StacksAndQueuesExercises/X05BalancedParentheses.java
@@ -1,4 +1,5 @@
-package JAStacksAndQueuesExercises;
+package JA02StacksAndQueuesExercises;
+
 /*
 Given a sequence consisting of parentheses, determine whether the expression is balanced. A sequence of parentheses is balanced if every open parenthesis can be paired uniquely with a closing parenthesis that occurs after the former. Also, the interval between them must be balanced.
 You will be given three types of parentheses: (, {, and [.
@@ -10,7 +11,6 @@ Input
 •	Each character of the sequence will be one of the following: {, }, (, ), [, ].
 Output
 •	For each test case, print on a new line "YES" if the parentheses are balanced. Otherwise, print "NO".
-
  */
 
 import java.util.ArrayDeque;
@@ -23,24 +23,26 @@ public class X05BalancedParentheses {
 
         ArrayDeque<Character> deque = new ArrayDeque<>();
         boolean areEquals = false;
+
         if (input.length() % 2 != 0) {
             System.out.println("NO");
         } else {
+
             for (int i = 0; i < input.length(); i++) {
                 char currentChar = input.charAt(i);
+
                 if (currentChar == '(' || currentChar == '{' || currentChar == '[') {
                     deque.push(currentChar);
                 } else {
+
                     if (deque.peek() == '(' && currentChar == ')') {
                         areEquals = true;
                         deque.pop();
                         continue;
-
                     } else if (deque.peek() == '[' && currentChar == ']') {
                         areEquals = true;
                         deque.pop();
                         continue;
-
                     } else if (deque.peek() == '{' && currentChar == '}') {
                         areEquals = true;
                         deque.pop();
@@ -56,6 +58,5 @@ public class X05BalancedParentheses {
         if (areEquals) {
             System.out.println("YES");
         }
-
     }
 }
