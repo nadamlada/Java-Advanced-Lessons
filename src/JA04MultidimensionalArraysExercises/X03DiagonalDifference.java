5538d35cbdf6f3170415c14c88d923408d92edf6
Nadezhda <nan.dudova@gmail.com>
2022-07-27 11:27:56 +0300

    commits

--- src/JA04MultidimensionalArraysExercises/X03DiagonalDifference.java
+++ src/JA04MultidimensionalArraysExercises/X03DiagonalDifference.java
@@ -1,4 +1,5 @@
-package JAMultidimensionalArraysExercises;
+package JA04MultidimensionalArraysExercises;
+
 /*
 Write a program that finds the difference between the sums of the square matrix diagonals (absolute value).
 
@@ -10,7 +11,6 @@ Hints
 •	Use a single loop i = [1 … n] to sum the diagonals.
 •	The primary diagonal holds all cells {row, col} where row == col == i.
 •	The secondary diagonal holds all cells {row, col} where row == i and col == n-1-i.
-
  */
 
 import java.util.Arrays;
@@ -22,20 +22,24 @@ public class X03DiagonalDifference {
         int size = Integer.parseInt(scanner.nextLine());
 
         int[][] matrix = new int[size][];
+
         for (int row = 0; row < size; row++) {
             int[] rowInput = Arrays.stream(scanner.nextLine().split("\\s")).mapToInt(Integer::parseInt).toArray();
+
             for (int col = 0; col < size; col++) {
                 matrix[row] = rowInput;
             }
         }
 
         int sumPrimaryDiagonal = 0;
+
         for (int row = 0; row < matrix.length; row++) {
             int currentIntersection1 = matrix[row][row];
             sumPrimaryDiagonal += currentIntersection1;
         }
 
         int sumSecondaryDiagonal = 0;
+
         for (int row = 0; row < matrix.length; row++) {
             int currentIntersection2 = matrix[row][matrix.length - 1 - row];
             sumSecondaryDiagonal += currentIntersection2;
