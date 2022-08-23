2f5556c7b004ecc3262cbd2ea5c7353a0b3823e9
Nadezhda <nan.dudova@gmail.com>
2022-08-23 12:43:51 +0300

    commits

--- src/JA03MultidimensionalArraysLab/L05MaximumSumOf_2x2Submatrix.java
+++ src/JA03MultidimensionalArraysLab/L05MaximumSumOf_2x2Submatrix.java
@@ -1,13 +1,13 @@
-package JAMultidimensionalArraysLab;
-
-import java.util.Arrays;
-import java.util.Scanner;
+package JA03MultidimensionalArraysLab;
 
 /*
 Write a program that reads a matrix from the console. Then find the biggest sum of a 2x2 submatrix. Print the submatrix and its sum.
 On the first line, you will get the dimensions of the matrix in the format "{rows, columns}". On the next lines, you will get the elements for each row separated by a comma.
-
  */
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class L05MaximumSumOf_2x2Submatrix {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -26,6 +26,7 @@ public class L05MaximumSumOf_2x2Submatrix {
         int[][] biggestMatrix = new int[2][2];
 
         for (int row = 0; row < rows - 1; row++) {
+
             for (int col = 0; col < cols - 1; col++) {
                 int currentSum = matrix[row][col]
                         + matrix[row][col + 1]
@@ -37,7 +38,7 @@ public class L05MaximumSumOf_2x2Submatrix {
                     biggestMatrix = new int[][]{
                             {matrix[row][col], matrix[row][col + 1]},
                             {matrix[row + 1][col], matrix[row + 1][col + 1]}
-                    };
+                    }
                 }
             }
         }
@@ -45,14 +46,14 @@ public class L05MaximumSumOf_2x2Submatrix {
         //print matrix
         for (int row = 0; row < 2; row++) {
             int[] arr = biggestMatrix[row];
+
             for (int i : arr) {
                 System.out.print(i + " ");
             }
+
             System.out.println();
         }
+
         System.out.println(maxSum);
     }
-
 }
-
-
