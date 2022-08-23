fce7a7f1c235081f42a2b97645c0b7e5df21a513
Nadezhda <nan.dudova@gmail.com>
2022-08-23 12:41:26 +0300

    commits

--- src/JA03MultidimensionalArraysLab/L03IntersectionOfTwoMatrices.java
+++ src/JA03MultidimensionalArraysLab/L03IntersectionOfTwoMatrices.java
@@ -1,12 +1,12 @@
-package JAMultidimensionalArraysLab;
-
-import java.util.Scanner;
+package JA03MultidimensionalArraysLab;
 
 /*
 Write a program that reads two char matrices (A[][] and B[][]) of the same order M * N and prints the third matrix C[][] which is filled with the intersecting elements of A and B, otherwise set the element to '*'. On the first two lines, you receive M and N, then on 2 * M lines N characters – the matrices elements.
 The matrix elements may be any ASCII char except '*'.
-
  */
+
+import java.util.Scanner;
+
 public class L03IntersectionOfTwoMatrices {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
