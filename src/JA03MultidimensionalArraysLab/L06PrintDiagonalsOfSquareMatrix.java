acb8b3fb2b9b09596c65d9bb2c5d2d4f3e556877
Nadezhda <nan.dudova@gmail.com>
2022-08-24 12:44:33 +0300

    commits

--- src/JA03MultidimensionalArraysLab/L06PrintDiagonalsOfSquareMatrix.java
+++ src/JA03MultidimensionalArraysLab/L06PrintDiagonalsOfSquareMatrix.java
@@ -1,4 +1,5 @@
-package JAMultidimensionalArraysLab;
+package JA03MultidimensionalArraysLab;
+
 /*
 Write a program that reads a matrix from the console. Then print the diagonals. The matrix will always be square. On the first line, you read a single integer N the matrix size. Then on each line N elements. The first diagonal should always start with the element at the first row and col, the second diagonal should start with the element at the last row and first col.
  */
@@ -12,6 +13,7 @@ public class L06PrintDiagonalsOfSquareMatrix {
 
         int n = Integer.parseInt(scanner.nextLine());
         int[][] matrix = new int[n][n];
+
         for (int row = 0; row < n; row++) {
             int[] current = Arrays.stream(scanner.nextLine().split("\\s"))
                     .mapToInt(Integer::parseInt)
@@ -22,6 +24,7 @@ public class L06PrintDiagonalsOfSquareMatrix {
         for (int i = 0; i < n; i++) {
             System.out.print(matrix[i][i] + " ");
         }
+
         System.out.println();
 
         for (int j = 0; j < n; j++) {
